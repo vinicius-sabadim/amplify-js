f7ce862969a98b7c64121bf771d668641f10d664
Jim Blanchard <jablanch@amazon.com>
2023-01-26 21:09:04 -0600

    Changes

--- packages/storage/src/s3/commands/get.ts
+++ packages/storage/src/s3/commands/get.ts
@@ -5,23 +5,21 @@ import {
 	GetObjectCommandOutput,
 } from '@aws-sdk/client-s3';
 import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner';
-import { createRequest } from '@aws-sdk/util-create-request';
-import { formatUrl } from '@aws-sdk/util-format-url';
-import { Credentials } from '@aws-amplify/core';
+import { getSignedParams } from '@aws-amplify/core';
 import { getPrefix } from '../../common/S3ClientUtils';
 import { S3ProviderGetConfig } from '../../types';
 import { getStorageConfig } from '../utils';
+import { generatePresignedUrl } from '../client';
 
 const DEFAULT_PRESIGN_EXPIRATION = 900;
 
 export const get = async (
 	key: string,
+	awsCreds: any,
 	config?: S3ProviderGetConfig,
 	sdkClientCreator?: (key: string, options: any) => Promise<S3Client> // S3 client escape hatch
-): Promise<string | GetObjectCommandOutput> => {
+): Promise<string | GetObjectCommandOutput | Response> => {
 	const s3GlobalConfig = getStorageConfig();
-	const credentials = await Credentials.get(); // TODO Replace with functional credential provider
-	const cred = Credentials.shear(credentials);
 	const options = Object.assign({}, s3GlobalConfig, config);
 	const {
 		bucket,
@@ -38,8 +36,9 @@ export const get = async (
 		SSECustomerKeyMD5,
 		progressCallback,
 	} = options;
-	const prefix = getPrefix({ ...options, credentials: cred }); // TODO Standardize prefix generation across APIs
+	const prefix = getPrefix({ ...options, credentials: awsCreds }); // TODO Standardize prefix generation across APIs
 	const final_key = prefix + key;
+	const path = `https://${options.bucket}.s3.${options.region}.amazonaws.com/${final_key}`;
 
 	// Build request & S3 command
 	const params: GetObjectCommandInput = {
@@ -58,24 +57,23 @@ export const get = async (
 	// Initialize client
 	const sdkClient = sdkClientCreator && (await sdkClientCreator(key, options));
 
-	// Check if we should download immediately
-	if (download) {
-		const command = new GetObjectCommand(params);
+	if (sdkClient) {
+		// Check if we should download immediately
+		if (download) {
+			//const command = new GetObjectCommand(params);
 
-		if (progressCallback) {
-			console.error('Progress callback not currently supported');
-		}
+			if (progressCallback) {
+				console.error('Progress callback not currently supported');
+			}
 
-		const response = sdkClient && (await sdkClient.send(command));
+			//const response = await sdkClient.send(command);
 
-		return response as GetObjectCommandOutput;
-	} else {
-		// TODO How to do this with custom client? Generate client config independently of having escape hatch?
-		if (sdkClient) {
+			//return response as GetObjectCommandOutput;
+		} else {
 			// If not downloading, generate a pre-signed URL
 			const s3ClientConfig = sdkClient.config;
 			const signer = s3ClientConfig && new S3RequestPresigner(s3ClientConfig);
-			const request = await createRequest(
+			/*const request = await createRequest(
 				sdkClient,
 				new GetObjectCommand(params)
 			);
@@ -85,7 +83,50 @@ export const get = async (
 				})
 			);
 
-			return url;
+			return url;*/
+		}
+	} else {
+		if (download) {
+			let headers = {
+				'X-Amz-Content-Sha256':
+					'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+			};
+
+			// Generate authorization headers
+			// TODO Merge this with http client
+			const signed_params = getSignedParams({
+				region: options.region,
+				params: {
+					method: 'GET',
+					data: null,
+					url: path,
+					headers,
+				},
+				credentials: {
+					...awsCreds,
+				},
+				service: 's3',
+			});
+			headers = { ...headers, ...signed_params.headers };
+
+			const response = await fetch(path, {
+				method: 'GET',
+				body: null,
+				mode: 'cors',
+				headers,
+			});
+
+			return response;
+		} else {
+			// Generate a pre-signed URL
+			const presignedUrl = await generatePresignedUrl(
+				path,
+				options.region,
+				awsCreds,
+				'GET'
+			);
+
+			return presignedUrl;
 		}
 	}
 
--- packages/storage/src/s3/commands/list.ts
+++ packages/storage/src/s3/commands/list.ts
@@ -3,8 +3,8 @@ import {
 	S3Client,
 	ListObjectsV2Command,
 } from '@aws-sdk/client-s3';
-import { Credentials } from '@aws-amplify/core';
-import { getPrefix, createS3Client } from '../../common/S3ClientUtils';
+import { getSignedParams } from '@aws-amplify/core';
+import { getPrefix } from '../../common/S3ClientUtils';
 import {
 	S3ProviderListConfig,
 	S3ClientOptions,
@@ -18,32 +18,96 @@ const queryS3 = async (
 	path: string,
 	params: ListObjectsV2Request,
 	options: S3ClientOptions,
-	client?: S3Client
+	awsCreds: any,
+	sdkClient?: S3Client
 ): Promise<S3ProviderListOutput> => {
-	const credentials = await Credentials.get(); // TODO Replace with functional credential provider
-	const cred = Credentials.shear(credentials);
-	const prefix = getPrefix({ ...options, credentials: cred }); // TODO Standardize prefix generation across APIs
+	const prefix = getPrefix({ ...options, credentials: awsCreds }); // TODO Standardize prefix generation across APIs
 	const final_path = prefix + path;
-	params.Prefix = final_path;
 
 	const listOutput: S3ProviderListOutput = {
 		results: [],
 		hasNextToken: false,
 	};
-	const listObjectsV2Command = new ListObjectsV2Command({ ...params });
-	const response = client && (await client.send(listObjectsV2Command));
 
-	if (response && response.Contents) {
-		listOutput.results = response.Contents.map(item => {
-			return {
-				key: item.Key?.substr(prefix.length),
-				eTag: item.ETag,
-				lastModified: item.LastModified,
-				size: item.Size,
-			};
+	if (sdkClient) {
+		//TODO Fix tree-shaking with SDK commands
+		//params.Prefix = final_path;
+		//const listObjectsV2Command = new ListObjectsV2Command({ ...params });
+		//const response = await sdkClient.send(listObjectsV2Command);
+		let response;
+
+		if (response && response.Contents) {
+			listOutput.results = response.Contents.map(item => {
+				return {
+					key: item.Key?.substr(prefix.length),
+					eTag: item.ETag,
+					lastModified: item.LastModified,
+					size: item.Size,
+				};
+			});
+			listOutput.nextToken = response.NextContinuationToken;
+			listOutput.hasNextToken = !!response.IsTruncated;
+		}
+	} else {
+		const path = `https://${options.bucket}.s3.${options.region}.amazonaws.com/?list-type=2&prefix=${prefix}`;
+
+		let headers = {
+			'Cache-Control': 'no-store',
+			'X-Amz-Content-Sha256':
+				'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+		};
+
+		// Generate authorization headers
+		// TODO Merge this with http client
+		const signed_params = getSignedParams({
+			region: options.region,
+			params: {
+				method: 'GET',
+				data: null,
+				url: path,
+				headers,
+			},
+			credentials: {
+				...awsCreds,
+			},
+			service: 's3',
 		});
-		listOutput.nextToken = response.NextContinuationToken;
-		listOutput.hasNextToken = !!response.IsTruncated;
+		headers = { ...signed_params.headers };
+
+		const results = await fetch(path, {
+			method: 'GET',
+			body: null,
+			mode: 'cors',
+			headers,
+		});
+
+		// Parse XML response
+		const responseBody = await results.text();
+		const parser = new DOMParser();
+		const xmlDoc = parser.parseFromString(responseBody, 'text/xml');
+
+		const xmlContents =
+			xmlDoc.firstElementChild?.getElementsByTagName('Contents');
+
+		if (xmlContents) {
+			listOutput.results = Array.from(xmlContents).map(element => {
+				return {
+					key: element.children[0].innerHTML,
+					eTag: element.children[2].innerHTML,
+					lastModified: new Date(element.children[1].innerHTML),
+					size: Number(element.children[3].innerHTML),
+				};
+			});
+
+			//console.log('+xml response', xmlDoc);
+
+			listOutput.hasNextToken =
+				!!xmlDoc.firstElementChild?.getElementsByTagName('IsTruncated')[0]
+					.innerHTML;
+			if (listOutput.hasNextToken) {
+				//listOutput.nextToken = xmlDoc.firstElementChild?.getElementsByTagName('NextToken')[0].innerHTML;
+			}
+		}
 	}
 
 	return listOutput;
@@ -51,6 +115,7 @@ const queryS3 = async (
 
 export const list = async (
 	path: string,
+	awsCreds: any,
 	config?: S3ProviderListConfig,
 	sdkClientCreator?: (key: string, options: any) => Promise<S3Client> // S3 client escape hatch
 ): Promise<S3ProviderListOutput> => {
@@ -76,7 +141,7 @@ export const list = async (
 	if (pageSize === 'ALL') {
 		// Automatically iterate over all pages
 		do {
-			queryResult = await queryS3(path, params, options, sdkClient);
+			queryResult = await queryS3(path, params, options, awsCreds, sdkClient);
 			listOutput.results.push(...queryResult.results);
 
 			if (queryResult.nextToken)
@@ -90,7 +155,7 @@ export const list = async (
 			console.error(`pageSize should be from 0 - ${MAX_PAGE_SIZE}.`);
 		}
 
-		queryResult = await queryS3(path, params, options, sdkClient);
+		queryResult = await queryS3(path, params, options, awsCreds, sdkClient);
 		listOutput.results.push(...listOutput.results);
 		listOutput.hasNextToken = listOutput.hasNextToken;
 		listOutput.nextToken = null ?? listOutput.nextToken;
